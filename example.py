03920885cebf547b01c48683ee2f42481ce1fee2
jasmoon <fourdusk4@gmail.com>
2019-04-14 01:31:02 +0800

    debug travis

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ from raise_player import Group18Player
 
 config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
 
-config.register_player(name="f1", algorithm=DumbHonestPlayer())
+config.register_player(name="f1", algorithm=Group18Player())
 config.register_player(name="FT2", algorithm=Group18Player())
 
 game_result = start_poker(config, verbose=1)
